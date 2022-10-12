6900142657d869e15cd965cee09b997f7df3ed39
cloudtestingph2 <109664751+cloudtestingph2@users.noreply.github.com>
2022-10-12 19:58:39 +0530

    Update special$$char@%#.sql

--- sql/special$$char@%#.sql
+++ sql/special$$char@%#.sql
@@ -2,7 +2,5 @@ create table special1 ( col1 number, col2 number)
 /
 insert into special1 values(10,10)
 /
-create table special2 ( col1 number, col2 number)
-/
-insert into special2 values(20,20)
+insert into special1 values(20,20)
 /
