287ec47eaec5fb24b931df9365199d77d6064dea
cloudtestingph2 <109664751+cloudtestingph2@users.noreply.github.com>
2022-10-20 02:32:37 +0530

    Update connect_file1.sql

--- sql/connect_file1.sql
+++ sql/connect_file1.sql
@@ -1,7 +1,7 @@
 
 connect sys/knl7 as sysdba
 /
-create table tab_connect ( col1 number, col2 number)
+create table tab_connect1 ( col1 number, col2 number)
 /
-insert into tab_connect values(10,10)
+insert into tab_connect1 values(10,10)
 /
