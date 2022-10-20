0b532f77349c71771974ae95be1f66987a6ff856
cloudtestingph2 <109664751+cloudtestingph2@users.noreply.github.com>
2022-10-20 11:00:41 +0530

    Update plsql_file1.sql

--- sql/plsql_file1.sql
+++ sql/plsql_file1.sql
@@ -1,17 +1,10 @@
 create table tab_plsql ( col1 number, col2 number)
-/
 begin
-/
-for i in 1..5
-/
-loop
-/
-insert into tab_plsql values(i+10,i+30)
-/
-end loop
-/
-end
-/
+ for i in 1..5
+ loop
+ insert into tab_plsql values(i+10,i+30);
+end loop;
+end;
 /
 select * from tab_plsql order by col1
 /
