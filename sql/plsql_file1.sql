81365e310d465217197a68c5ad26c89a706746e2
cloudtestingph2 <109664751+cloudtestingph2@users.noreply.github.com>
2022-10-21 21:22:32 +0530

    Update plsql_file1.sql

--- sql/plsql_file1.sql
+++ sql/plsql_file1.sql
@@ -1,15 +1,15 @@
-create table tab_plsql ( col1 number, col2 number)
+create table tab_plsql1 ( col1 number, col2 number)
 /
 declare
 var1 number :=10;
 begin
  for i in 1..5
  loop
-   insert into tab_plsql values(i+10,i+30);
+   insert into tab_plsql1 values(i+10,i+30);
  end loop;
 dbms_output.put_line('Variable value is ' || var1); 
 end;
 /
-select * from tab_plsql order by col1
+select * from tab_plsql1 order by col1
 /
 
