a16dcb3c580366bcd5d78cec9ab9f76b4f1b9970
cloudtestingph2 <109664751+cloudtestingph2@users.noreply.github.com>
2022-10-21 13:44:47 +0530

    Update plsql_file1.sql

--- sql/plsql_file1.sql
+++ sql/plsql_file1.sql
@@ -5,8 +5,8 @@ var1 number :=10;
 begin
  for i in 1..5
  loop
- insert into tab_plsql values(i+10,i+30);
-end loop;
+   insert into tab_plsql values(i+10,i+30);
+ end loop;
 dbms_output.put_line('Variable value is ' || var1); 
 end;
 /
